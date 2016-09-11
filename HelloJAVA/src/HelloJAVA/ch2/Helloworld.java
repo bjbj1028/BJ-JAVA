944ff8d19734a44c2ed3f0566f381bff37712241
bjbj1028 <bjbj1028@naver.com>
2016-09-11 20:48:33 +0900

    Update Helloworld.java
    for pull

--- HelloJAVA/src/HelloJAVA/ch2/Helloworld.java
+++ HelloJAVA/src/HelloJAVA/ch2/Helloworld.java
@@ -6,6 +6,8 @@ public class Helloworld {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow World");
 		System.out.println("Wellcome");
+		System.out.println("Wellcome BJ");
+		
 	}
 
 }
